b21dff3df855a0019168ffd41d539fc686a79dfc
Maysam Torabi <kherad@gmail.com>
2020-02-23 23:26:23 +0330

    fixing Error: package.json does not exist at /package.json

--- next.config.js
+++ next.config.js
@@ -196,4 +196,38 @@ const nextConfig = {
   },
 }
 
-module.exports = withOffline(withBundleAnalyzer(nextConfig))
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
+
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false,
+        })
+
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: '@zeit/webpack-asset-relocator-loader',
+            options: {
+              outputAssetBase: 'assets',
+              existingAssetNames: [],
+              wrapperCompatibility: true,
+              escapeNonAnalyzableRequires: true,
+            },
+          },
+        })
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    },
+  })
+}
+
+module.exports = withAssetRelocator(withOffline(withBundleAnalyzer(nextConfig)))
